3c91ff4966b3a4cd66a4a2b3fcce022d5efac95d
Solmaz-Salehzadeh <solmaz.salehzadeh1369@gmail.com>
2022-11-05 00:04:20 +0330

    question 1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+// question 1
+let salaries = {
+    "jhone":100,
+    "pete":300,
+    "mary":250,
+    "soli":300,
+}
+const test = Object.values(salaries)
+const x = Math.max(...test)
+for (const [key, value] of Object.entries(salaries)) {
+  if(value === x ){console.log(key)}
+}
