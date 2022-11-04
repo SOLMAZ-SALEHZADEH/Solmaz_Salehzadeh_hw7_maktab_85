0e6c79ccb54cf350f5874f26c73f966a8da4242d
Solmaz-Salehzadeh <solmaz.salehzadeh1369@gmail.com>
2022-11-05 00:12:01 +0330

    question 3

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// question 2
-function sortDescending (...arguments){
-    const data = [...arguments]
-   const arr=[]
-   for (let i=0;i<data.length;i++){
-    arr.push(...arguments[i])
-   }
-  const result =  arr.sort((a,b)=>a-b)
-  console.log(result)
+// question 3
+const unique = (array)=>{
+    return [...new Set(array)]
 }
-sortDescending([1,2,3,9,10,12],[4,5,6,7,8,13,14,15])
+let chars = ['1', '1', 'hare', 'krishns', 'krishns',":-o"];
+console.log(unique(chars));
