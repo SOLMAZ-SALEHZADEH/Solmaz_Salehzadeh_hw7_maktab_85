684f7645a6a06ab910c4767ebcefd8504cfd22f1
Solmaz-Salehzadeh <solmaz.salehzadeh1369@gmail.com>
2022-11-05 00:14:20 +0330

    question 4

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-// question 3
-const unique = (array)=>{
-    return [...new Set(array)]
-}
-let chars = ['1', '1', 'hare', 'krishns', 'krishns',":-o"];
-console.log(unique(chars));
+// question4
+function sortStrChars(str) {
+    if (!str) {
+      return;
+    }
+    str = str.split('');
+    str = str.sort();
+    str = str.join('');
+    return str;
+  }
+  
+  const words = ["dell", "ledl", "abc", "cba", 'boo'];
+  
+  function getGroupedAnagrams(words) {
+    const anagrams = {}; // {abc:[abc,cba], dell:[dell, ledl]}
+    words.forEach((word) => {
+      const sortedWord = sortStrChars(word);
+      if (anagrams[sortedWord]) {
+        return anagrams[sortedWord].push(word);
+      }
+      anagrams[sortedWord] = [word];
+    });
+    return anagrams;
+  }
+  const arr = []
+  const groupedAnagrams = getGroupedAnagrams(words);
+  for (const sortedWord in groupedAnagrams) {
+    arr.push(groupedAnagrams[sortedWord][0])
+  }
+  console.log(arr)
