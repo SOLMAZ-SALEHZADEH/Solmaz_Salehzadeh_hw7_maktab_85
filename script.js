780d5a5c4c3241f6417df60bff159aa867f13cc2
Solmaz-Salehzadeh <solmaz.salehzadeh1369@gmail.com>
2022-11-05 00:17:38 +0330

    question5

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-// question4
-function sortStrChars(str) {
-    if (!str) {
-      return;
-    }
-    str = str.split('');
-    str = str.sort();
-    str = str.join('');
-    return str;
-  }
-  
-  const words = ["dell", "ledl", "abc", "cba", 'boo'];
-  
-  function getGroupedAnagrams(words) {
-    const anagrams = {}; // {abc:[abc,cba], dell:[dell, ledl]}
-    words.forEach((word) => {
-      const sortedWord = sortStrChars(word);
-      if (anagrams[sortedWord]) {
-        return anagrams[sortedWord].push(word);
-      }
-      anagrams[sortedWord] = [word];
-    });
-    return anagrams;
-  }
-  const arr = []
-  const groupedAnagrams = getGroupedAnagrams(words);
-  for (const sortedWord in groupedAnagrams) {
-    arr.push(groupedAnagrams[sortedWord][0])
-  }
-  console.log(arr)
+// question5
+let map = new Map();
+map.set("name","jhone")
+map.set("last","salehi")
+map.set("midd","soli")
+let keys=map.keys()
+console.log(keys)
+// map does key in not an array so push method cannot be applide
